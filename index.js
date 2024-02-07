1c814add427d0809ea260623cafff76a572e7426
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-02-07 16:18:33 +0200

    import&export

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Revision');
+import {addition} from './addition.js'
+// import {subtraction} from './subtraction.js'
+
+// console.log(subtraction(13,5));
+// console.log(subtraction(5,13));
+console.log(addition(20, 36));
--- subtraction.js
+++ subtraction.js
@@ -1,10 +1,10 @@
-function subtraction(a, b) {
-    return a - b
-}
+// function subtraction(a, b) {
+//     return a - b
+// }
 
-module.exports = {
-    subtraction
-}
+// module.exports = {
+//     subtraction
+// }
 
 // exports.addition = (a, b)=>{return a - b}
 // return b > a ? `${b} is greater than ${a}` : a - b
